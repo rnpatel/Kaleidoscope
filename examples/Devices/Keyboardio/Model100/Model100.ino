1047065bfde4150782504097a9f7b2f5191fb133
Rajiv Patel <2078885+rnpatel@users.noreply.github.com>
2022-10-05 14:35:02 -0400

    fixed tapDanceAction signature

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -215,7 +215,7 @@ KEYMAPS(
         Key_Escape,      Key_1, Key_2, Key_3, Key_4, Key_5, KC_MEH,
         Key_Backtick,    Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
         Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,
-        Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftBracket,
+        Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, TD(TDLBR),
 
         CTL_T(Spacebar), OSM(LeftAlt), OSM(LeftGui), OSM(LeftShift),
 
@@ -243,7 +243,7 @@ KEYMAPS(
         KC_HYP,    Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_Backspace,
         Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
                    Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
- Key_RightBracket, Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+        TD(TDRBR), Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
 
         OSM(RightAlt), OSM(RightGui), Key_Backslash, Key_Spacebar,
 
@@ -287,7 +287,7 @@ KEYMAPS(
 /* Re-enable astyle's indent enforcement */
 // clang-format on
 
-void tapDanceAction(uint8_t tap_dance_index, byte row, byte col, uint8_t tap_count,
+void tapDanceAction(uint8_t tap_dance_index, KeyAddr key_addr, uint8_t tap_count,
                     kaleidoscope::plugin::TapDance::ActionType tap_dance_action)
 {
     switch (tap_dance_index)

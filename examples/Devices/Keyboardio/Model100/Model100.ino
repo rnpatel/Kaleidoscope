a5973de26c2755b3de95cdce4c44237e22cba0c9
Rajiv Patel <2078885+rnpatel@users.noreply.github.com>
2023-11-30 11:25:43 -0500

    minimal
    - replaced thumb shift with fn (globe)
    - made left pinky shift OSM

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -206,7 +206,7 @@ KEYMAPS(
         // ├───────┼───────┼───────┼───────┼───────┼───────┼───────┤
         // │   ↑   │   Z   │   X   │   C   │   V   │   B   │ (→[→{ │
         // └───────┴───────┴───────┴───────┴─────┬─┴─────┬─┴─────┬─┴─────┬───────┐
-        //                                       │  ^/_  │   ⌥   │   ⌘   │   ↑   │
+        //                                       │  ^/_  │   ⌥   │   ⌘   │   ✩   │
         //                                       └───────┴───────┴───────┴───────┘
         //                                                   ┌───────┐
         //                                                   │  FUN  │
@@ -215,9 +215,9 @@ KEYMAPS(
         Key_Escape,      Key_1, Key_2, Key_3, Key_4, Key_5, KC_MEH,
         Key_Backtick,    Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
         Key_LeftControl, Key_A, Key_S, Key_D, Key_F, Key_G,
-        Key_LeftShift,   Key_Z, Key_X, Key_C, Key_V, Key_B, TD(TDLBR),
+        OSM(LeftShift),  Key_Z, Key_X, Key_C, Key_V, Key_B, TD(TDLBR),
 
-        CTL_T(Spacebar), OSM(LeftAlt), OSM(LeftGui), OSM(LeftShift),
+        CTL_T(Spacebar), OSM(LeftAlt), OSM(LeftGui), Consumer_AC_NextKeyboardLayoutSelect,
 
         ShiftToLayer(FUNCTION),
 
